151175ff40ba4f51d1082909c32c5dc05bd93d1a
Mike Rehberg <Coder@TR182.com>
2019-12-25 07:57:40 -0700

    Added AHRS decode for Time, Pitch, Roll, Heading, IAS, TAS and pALT

--- StringExample.py
+++ StringExample.py
@@ -2,7 +2,13 @@
 
 text = input()
 if (text.startswith('!1')):
-    print(text[2:4])
+    print('Time:    ', text[3:11])
+    print('Pitch:  ', text[11:15])
+    print('Roll:   ', text[15:20])
+    print('Heading: ', text[20:23])
+    print('IAS:     ',  text[23:27])
+    print('pALT:   ',  text[27:33])
+    print('TAS:     ', text[52:56])
 elif (text.startswith('!2')):
  print(text)
 
